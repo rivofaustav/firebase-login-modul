4431117587e956bd27b1ae10cba0f9eed34ad76d
rivofaustav <rivofaustav.rv@gmail.com>
2020-04-23 12:00:15 +0700

    edit action and add reducers

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -29,3 +29,71 @@ const loginError = () => {
         type: LOGIN_FAILURE
     };
 };
+
+const requestLogout = () => {
+    return {
+        type: LOGOUT_REQUEST
+    };
+};
+
+const receiveLogin = () => {
+    return {
+        type: LOGOUT_SUCCESS
+    };
+};
+
+const logoutError = () => {
+    return {
+      type: LOGOUT_FAILURE
+    };
+  };
+  
+  const verifyRequest = () => {
+    return {
+      type: VERIFY_REQUEST
+    };
+  };
+  
+  const verifySuccess = () => {
+    return {
+      type: VERIFY_SUCCESS
+    };
+  };
+  
+  export const loginUser = (email, password) => dispatch => {
+    dispatch(requestLogin());
+    myFirebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(user => {
+        dispatch(receiveLogin(user));
+      })
+      .catch(error => {
+        //Do something with the error if you want!
+        dispatch(loginError());
+      });
+  };
+  
+  export const logoutUser = () => dispatch => {
+    dispatch(requestLogout());
+    myFirebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch(receiveLogout());
+      })
+      .catch(error => {
+        //Do something with the error if you want!
+        dispatch(logoutError());
+    });
+};
+
+export const verifyAuth = () => dispatch => {
+  dispatch(verifyRequest());
+  myFirebase.auth().onAuthStateChanged(user => {
+    if (user !== null) {
+      dispatch(receiveLogin(user));
+    }
+    dispatch(verifySuccess());
+  });
+};
